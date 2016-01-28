94cfc9f70c2a43ceb0e0859fc51a2d9a5b434061
arundas <arundas@qburst.com>
2016-01-28 15:50:08 +0530

    Settings update

--- movies_for_all/movies_for_all/settings.py
+++ movies_for_all/movies_for_all/settings.py
@@ -52,6 +52,11 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'app_user.rest_exception.custom_exception_handler'
 }
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
